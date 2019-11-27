e93cce03fd79a982d2372dda7906474b58e73cec
Erik Forsberg <erikforsberg95@hotmail.com>
2019-11-27 11:46:09 +0100

    added simulate.py

--- model.py
+++ model.py
@@ -3,15 +3,23 @@ import numpy as np
 class Model:
 
     def __init__(self, lattice_size):
-        self.lattice_size = lattice_size
-        self.lattice = np.zeros([lattice_size, lattice_size])
+        self.lattice_size = lattice_size*lattice_size
+        self.lattice = np.zeros([lattice_size*lattice_size])
 
 
 
-    def initialice(self, mode):
+    def initialize(self, mode):
         if mode == 'hot':
-            return 1
+            indexes = np.random.choice(list(range(self.lattice_size)), size=3, replace=False)
+            for i in indexes:
+                self.lattice[i] = 1
         elif mode == 'cold':
             return 2
         else:
             return "mode must be set to cold or hot!"
+
+    def sweep(self, nr_sweeps):
+        
+
+    def compute_observables(self):
+        return
--- program.py
+++ program.py
@@ -1,13 +1,18 @@
 import initialize as init
 import model as md
+import simulate as sim
 
 
 def main():
+    ## Global parameters
+    q = [2, 4, 8, 20]
+    s = [8, 16, 32]
+
     model = md.Model(8)
     print(model.lattice_size)
     print(model.lattice)
-    i = model.initialice('codeld')
-    print(i)
+    model.initialize('hot')
+    print(model.lattice)
 
 if __name__ == '__main__':
     main()
